a85f0812659cf24daa90d9624eb908f3d685c01f
MagicTINTIN <compte.poubelle.georges@gmail.com>
2023-03-27 12:14:17 +0200

    ça maaarche

--- manual.js
+++ manual.js
@@ -1,16 +1,14 @@
-aloop = 0;
-xvar = 0;
-pas = .5;
-for (let index = 0; index < 10 * Math.cos(xvar); index++) {
-    setTimeout(() => {
-        envoi_message("perdu", { "session": sessionNB, "numero": qnb }, { "UUID": UUIDuser, "role": "A" });
-    }, 500);
-}
-xvar += pas;
+// Open your js console and copy paste
+
 loopFunction = setInterval(() => {
-    for (let index = 0; index < 10 * Math.cos(xvar); index++) {
-        setTimeout(() => {
-            envoi_message("perdu", { "session": sessionNB, "numero": qnb }, { "UUID": UUIDuser, "role": "A" });
-        }, 500);
-    }
-}, 30000);
+    
+    envoi_message("perdu", { "session": iquiz.question.session, "numero": iquiz.question.numero }, { "UUID": iquiz.data.UUID, "role": "A" });
+    
+}, 200);
+
+
+loopFunction = setInterval(() => {
+    envoi_message("reponse",{"session":iquiz.question.session,"numero":iquiz.question.numero,"ordre":iquiz.question.ordre},{"UUID":iquiz.data.UUID,"role":iquiz.data.role,"nbr_reponses":iquiz.data.nbr_reponses,"reponses":[2,4],"nature":"Q"})
+}, 200);
+
+clearInterval(loopFunction);
