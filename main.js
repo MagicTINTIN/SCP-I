20371f5542bec1aff43a9c11047ac38a92deada6
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2024-02-09 10:31:10 +0100

    there were still some mistakes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // When you are connected to your iquiz session, open your js console in your webbrowser (might be ctrl+maj+i or f12) and copy paste theses functions below
-// v - v - v - v - v
+//////////////////// Functions to copy \\\\\\\\\\\\\\\\\\\\ 
 function jeSuisPerdu(nb, time) {
 
     var rep = 0;
@@ -40,11 +40,14 @@ function fctPerdu(fct, offset, xmulti, ymulti, maxsteps) {
 
 //////////////////// End of functions \\\\\\\\\\\\\\\\\\\\ 
 /*
- * Now you can just type jeSuisPerdu(10, 4500) to send 10 times "Je suis perdu" in 4,5 seconds
- * If you want you can use jeSuisPerdu(Math.sin,10,0.8,5, 30) to create a beautiful wave of lost students between 5 and 15 students each minute during 30 minutes
- * 
- * You can also type jeReponds(35, 5000, [3]) to send 35 answer 3 in 5 seconds
+ * ANSWERING QUESTIONS
+ * You can type jeReponds(35, 5000, [3]) to send 35 answer 3 in 5 seconds
  * If you want to answer multiple answer like 1 and 3, just type jeReponds(35, 5000, [1,3])
+ *
+ *
+ * I'M LOST BUTTON
+ * You can just type jeSuisPerdu(10, 4500) to send 10 times "Je suis perdu" in 4,5 seconds
+ * If you want you can use fctPerdu(Math.sin,10,0.8,5, 30) to create a beautiful wave of lost students between 5 and 15 students each minute during 30 minutes
  * 
  * You can add other functions after as done below and call the functions as you want in fctPerdu, just replace Math.sin by your function name.
  * for instance :
